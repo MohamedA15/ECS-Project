913d19808d030930d776031a4e41aad1055cf138
MohamedA15 <mo684073@gmail.com>
2025-11-03 17:34:57 +0000

    fixed output names

--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -5,27 +5,27 @@ output "vpc_id" {
 output "public_subnet_ids" {
   value = aws_subnet.public[*].id
 }
-output "igw_id" {
-  value = aws_internet_gateway.igw.id
+
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
 }
 
-output "private_subnet_id" {
-  value = aws_subnet.private[*].id
-  
-} 
+output "public_route_table_id" {
+  value = aws_route_table.public_rt.id
+}
 
-output "route_table_id" {
-  value = aws_route_table.public_rt
-  
+output "private_route_table_id" {
+  value = aws_route_table.private_rt.id
+}
 
+output "igw_id" {
+  value = aws_internet_gateway.igw.id
 }
 
 output "nat_gateway_id" {
   value = aws_nat_gateway.nat.id
 }
 
-output "nat_eip_id" {
+output "nat_eip_ids" {
   value = aws_eip.nat_eip[*].id
-  
 }
-
