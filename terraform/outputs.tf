82a4f5b8537222ddc7228138d2d40feb008dc2ae
MohamedA15 <mo684073@gmail.com>
2025-11-16 16:43:29 +0000

    Add  ALB and ECS security group IDs to be used in other modules

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -9,3 +9,12 @@ output "public_subnet_ids" {
 output "private_subnet_ids" {
   value = module.vpc.private_subnet_ids
 }
+
+
+output "alb_sg_id" {
+  value = module.security_groups.alb_sg_id
+}
+
+output "ecs_sg_id" {
+  value = module.security_groups.ecs_sg_id
+}
