6c6e154f4ec27f4ff235c60103bc8bef398f64ab
MohamedA15 <mo684073@gmail.com>
2025-11-16 16:24:14 +0000

    Implement complete SG module including ALB and ECS security groups

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,3 +8,14 @@ module "vpc" {
   availability_zones   = var.availability_zones
   create_nat_eip       = var.create_nat_eip
 }
+
+
+module "security_groups" {
+  source = "./modules/sg"
+
+  alb_sg_name          = "alb-security-group"
+  ecs_sg_name          = "ecs-fargate-security-group"
+  vpc_id               = module.vpc.vpc_id
+  allow_http_from_cidr = "0.0.0.0/0"
+  allow_https_from_cidr = "0.0.0.0/0"
+}
