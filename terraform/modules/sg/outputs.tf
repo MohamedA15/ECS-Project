6fb6f38650e3a05e6f8adfc79494bbbdb2203ce2
MohamedA15 <mo684073@gmail.com>
2025-11-29 12:22:27 +0000

    added rds and redis outputs

--- terraform/modules/sg/outputs.tf
+++ terraform/modules/sg/outputs.tf
@@ -8,3 +8,11 @@ output "ecs_sg_id" {
   description = "The ID of the ECS security group"
   
 }
+
+output "rds_sg_id" {
+  value = aws_security_group.rds_sg.id
+}
+
+output "redis_sg_id" {
+  value = aws_security_group.redis_sg.id
+}
